037169a5d7922c5db7efc1f1331b5c3cd6bce15b
Yihao-Shi <syh-1999@outlook.com>
2024-12-11 16:30:10 -0500

    update dependencies

--- requirements.sh
+++ requirements.sh
@@ -1,2 +1,2 @@
 python3 -m pip install --upgrade pip
-python3 -m pip install taichi==1.6.0 scipy trimesh imageio matplotlib psutil pynvml==11.4.1 shapely==1.8 
+python3 -m pip install taichi==1.6.0 scipy trimesh imageio matplotlib psutil pynvml==11.4.1 shapely==1.8 meshio rtree scikit-image open3d
